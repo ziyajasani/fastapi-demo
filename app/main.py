0a7c2336bf8bbc0b7de6830cc9d530e7a1c2b6c7
Ziya Jasani <ziyajasani@Ziyas-MacBook-Pro.local>
2024-12-05 13:41:11 -0500

    saving changes

--- app/main.py
+++ app/main.py
@@ -1,65 +1,29 @@
 #!/usr/bin/env python3
 
+
+from fastapi import FastAPI
+from typing import Optional
+from pydantic import BaseModel
 import json
 import os
-from fastapi import FastAPI
-import mysql.connector
-from mysql.connector import Error
 
 app = FastAPI()
-from fastapi.middleware.cors import CORSMiddleware
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
-DBHOST = "ds2022.cqee4iwdcaph.us-east-1.rds.amazonaws.com"
-DBUSER = "admin"
-DBPASS = os.getenv('DBPASS')
-DB = "nem2p"
 
 @app.get("/")  # zone apex
 def zone_apex():
+    return {"Hello": "World"}
     return {"Good Day": "Sunshine!"}
 
-@app.get('/genres')
-async def get_genres():
-    db = mysql.connector.connect(user=DBUSER, host=DBHOST, password=DBPASS, database=DB, ssl_disabled=True)
-    cur = db.cursor()
-    query = "SELECT * FROM genres ORDER BY genreid;"
-    try:    
-        cur.execute(query)
-        headers=[x[0] for x in cur.description]
-        results = cur.fetchall()
-        json_data=[]
-        for result in results:
-            json_data.append(dict(zip(headers,result)))
-        return(json_data)
-    except Error as e:
-        print("MySQL Error: ", str(e))
-        return {"Error": "MySQL Error: " + str(e)}
-    finally:
-        cur.close()
-        db.close()
+@app.get("/add/{a}/{b}")
+@app.get("/sum/{a}/{b}")
+def add(a: int, b: int):
+    return {"sum": a + b}
 
-@app.get('/songs')
-async def get_genres():
-    db = mysql.connector.connect(user=DBUSER, host=DBHOST, password=DBPASS, database=DB, ssl_disabled=True)
-    cur = db.cursor()
-    query = "SELECT songs.title, songs.album, songs.artist, songs.year, songs.file, songs.image, genres.genre FROM songs JOIN genres WHERE songs.genre = genres.genreid ORDER BY songs.title;"
-    try:
-        cur.execute(query)
-        headers=[x[0] for x in cur.description]
-        results = cur.fetchall()
-        json_data=[]
-        for result in results:
-            json_data.append(dict(zip(headers,result)))
-        return(json_data)
-    except Error as e:
-        print("MySQL Error: ", str(e))
-        return None
-    finally:
-        cur.close()
-        db.close()
+@app.get("/multiply/{c}/{d}")
+def multiply(c: int, d: int):
+    return {"product": c * d}
+
+@app.get("/square/{e}")
+def square(e: int):
+    return {"square": e * e) 
+
